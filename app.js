c2713193275b73355e5a1ab8afbeb23a1a22ba58
Adrian Pabe <adrianpabs14@gmail.com>
2021-01-22 00:54:10 +0800

    Add tabs

--- app.js
+++ app.js
@@ -155,3 +155,23 @@ searchBar.addEventListener('keyup', (e) => {
     })
 })
 
+// **************************************************************************************************************** //
+
+// TABBED CONTENT
+
+const tabs = document.querySelector('.tabs');
+const panels = document.querySelectorAll('.panel');
+
+tabs.addEventListener('click', (e) => {
+    if(e.target.tagName === 'LI'){
+        const targetPanel = document.querySelector(e.target.dataset.target);
+        panels.forEach((panel) => {
+            if(panel === targetPanel){
+                panel.classList.add('active');
+            }else{
+                panel.classList.remove('active');
+            }
+        })
+    }
+})
+
