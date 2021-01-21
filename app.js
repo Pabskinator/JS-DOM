9856e9438821b26ecac3a8b9bdd438b37a397972
Adrian Pabe <adrianpabs14@gmail.com>
2021-01-21 22:39:27 +0800

    Log Query Selectors

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//converting to array
+// converting to array
 
 let titles = document.getElementsByClassName('title');
 
@@ -6,3 +6,18 @@ console.log(Array.isArray(titles));
 console.log(Array.isArray(Array.from(titles)));
 
 Array.from(titles).forEach((item) => console.log(item));
+
+// **************************************************************************************************************** //
+
+// Query Selector
+
+const wrap = document.querySelector('#wrapper');
+console.log(wrap);
+
+// Pseudo class
+const booklistSecondChild = document.querySelector('#book-list li:nth-child(2) .name');
+console.log(booklistSecondChild);
+
+// Select multiple elements
+let books = document.querySelectorAll('#book-list li .name');
+console.log(books);
